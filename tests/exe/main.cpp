d0c0c8dfa6897f9336b3b386417d2c0c153ad973
Sine Striker <trueful@163.com>
2023-03-12 16:31:47 +0800

    Fix test exe

--- tests/exe/main.cpp
+++ tests/exe/main.cpp
@@ -3,36 +3,40 @@
 #include <loadso/library.h>
 #include <loadso/system.h>
 
+using namespace LoadSO;
+
 int main(int argc, char *argv[]) {
-    LoadSO::System::PrintLine(LOADSO_STR("[Test Path]"));
-    LoadSO::System::PrintLine(LOADSO_STR("App Path       : ") + LoadSO::System::ApplicationPath());
-    LoadSO::System::PrintLine(LOADSO_STR("App File Name  : ") + LoadSO::System::ApplicationFileName());
-    LoadSO::System::PrintLine(LOADSO_STR("App Directory  : ") + LoadSO::System::ApplicationDirectory());
-    LoadSO::System::PrintLine(LOADSO_STR("App Name       : ") + LoadSO::System::ApplicationName());
+    System::PrintLine(LOADSO_STR("[Test Path]"));
+    System::PrintLine(LOADSO_STR("App Path       : ") + System::ApplicationPath());
+    System::PrintLine(LOADSO_STR("App File Name  : ") + System::ApplicationFileName());
+    System::PrintLine(LOADSO_STR("App Directory  : ") + System::ApplicationDirectory());
+    System::PrintLine(LOADSO_STR("App Name       : ") + System::ApplicationName());
 
     // Load library
-    LoadSO::System::PrintLine(LOADSO_STR("[Test Load Library]"));
-    LoadSO::Library lib;
-    if (!lib.open(LOADSO_STR(
-#ifndef _WIN32
-        "../lib/"
+    System::PrintLine(LOADSO_STR("[Test Load Library]"));
+    Library lib;
+
+    if (!lib.open(
+#ifdef _WIN32
+            LOADSO_STR(DLL_NAME)
+#else
+            LOADSO_STR("../lib/" DLL_NAME)
 #endif
-        DLL_NAME
-        ))) {
-        LoadSO::System::ShowError(lib.lastError());
+                )) {
+        System::ShowError(lib.lastError());
         return -1;
     }
-    LoadSO::System::PrintLine(LOADSO_STR("OK"));
+    System::PrintLine(LOADSO_STR("OK"));
 
     // Get function
-    LoadSO::System::PrintLine(LOADSO_STR("[Test Get Function]"));
+    System::PrintLine(LOADSO_STR("[Test Get Function]"));
     using AddFunc = int (*)(int, int);
     auto add_func = (AddFunc) lib.entry("add");
     if (!add_func) {
-        LoadSO::System::ShowError(lib.lastError());
+        System::ShowError(lib.lastError());
         return -1;
     }
-    LoadSO::System::PrintLine(LOADSO_STR("OK"));
+    System::PrintLine(LOADSO_STR("OK"));
 
     // Call function
     std::cout << add_func(1, 3) << std::endl;
